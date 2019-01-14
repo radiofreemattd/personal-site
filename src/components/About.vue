b2fa6d973bc380208c6c7d4d9fd556d94554e0a7
Matthew Dunn <mshieldsdunn@gmail.com>
2019-01-14 13:00:56 -0500

    Add support for AWS deployment
    This commit adds support for automated deployment via AWS

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    title: String,
+    subtitle: String
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <About title="This is a title" subtitle="This is a subtitle"></About>
 </template>
+
+<script>
+import About from '@/components/About.vue'
+export default {
+  name: 'about',
+  components: {
+    About
+  }
+}
+</script>
+
